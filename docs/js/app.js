a8d9f10d5ca7b210fa915e19914ebbda2319eab0
sue445 <sue445@sue445.net>
2020-02-19 01:02:09 +0900

    Refactor with vue.js

---
+++ docs/js/app.js
@@ -0,0 +1,48 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    connpass_groups: [],
+    doorkeeper_groups: []
+  },
+  methods: {
+    getConnpassGroups: function () {
+      var url = 'config/connpass.json';
+      var that = this;
+      axios.get(url).then(function (x) {
+        that.connpass_groups = x.data.groups.map(function (group) {
+          group.url = "https://" + group.id + ".connpass.com/";
+          return group;
+        }).sort(function (a, b) {
+          return that.compareString(a.name, b.name);
+        });
+      });
+    },
+
+    getDoorkeeperGroups: function () {
+      var url = 'config/doorkeeper.json';
+      var that = this;
+      axios.get(url).then(function (x) {
+        that.doorkeeper_groups = x.data.groups.map(function (group) {
+          group.url = "https://" + group.id + ".doorkeeper.jp/";
+          return group;
+        }).sort(function (a, b) {
+          return that.compareString(a.name, b.name);
+        });
+      });
+    },
+
+    compareString: function (a, b) {
+      if (a < b) {
+        return -1;
+      }
+      if (a > b) {
+        return 1;
+      }
+      return 0;
+    }
+  },
+  mounted: function () {
+    this.getConnpassGroups();
+    this.getDoorkeeperGroups();
+  }
+});
