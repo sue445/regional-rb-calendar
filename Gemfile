445c288a422372692e4da2dcdf2cfc5e3992f07f
sue445 <sue445@sue445.net>
2021-06-12 18:18:21 +0900

    Impl generate_ical_from_connpass

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem "connpass"
 gem "icalendar"
 gem "parallel"
 gem "puma", require: false
--- spec/lib/event_calendar_spec.rb
+++ spec/lib/event_calendar_spec.rb
@@ -74,4 +74,178 @@ describe EventCalendar do
 
     it { should eq ical.gsub("\n", "\r\n") }
   end
+
+  describe "#generate_ical_from_connpass" do
+    subject { calendar.generate_ical_from_connpass(groups, current_date) }
+
+    let(:groups) do
+      [
+        {
+          "id" => "hirakatarb",
+          "name" => "Hirakata.rb",
+          "series_id" => 11126,
+        },
+        {
+          "id" => "toyamarb",
+          "name" => "Toyama.rb",
+          "series_id" => 2557,
+        },
+      ]
+    end
+
+    let(:current_date) { Date.parse("2021-06-12") }
+
+    before do
+      stub_request(:get, "http://connpass.com/api/v1/event/?count=100&order=1&series_id=11126,2557&ym=202103,202104,202105,202106,202107,202108,202109").
+        to_return(body: File.new("#{spec_dir}/fixtures/connpass.json"))
+    end
+
+    let(:ical) do
+      <<~ICAL
+        BEGIN:VCALENDAR
+        VERSION:2.0
+        PRODID:icalendar-ruby
+        CALSCALE:GREGORIAN
+        METHOD:PUBLISH
+        X-WR-CALNAME;VALUE=TEXT:connpassのイベント
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/206395/
+        DTSTART:20210305T233000
+        DTEND:20210306T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/206395/
+        SUMMARY:Hirakata.rb #6
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/206846/
+        DTSTART:20210312T233000
+        DTEND:20210313T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/206846/
+        SUMMARY:Hirakata.rb #7
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://toyamarb.connpass.com/event/204596/
+        DTSTART:20210313T040000
+        DTEND:20210313T090000
+        DESCRIPTION:https://toyamarb.connpass.com/event/204596/
+        SUMMARY:Toyama.rb #61 もくもく会オンライン
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/207932/
+        DTSTART:20210319T233000
+        DTEND:20210320T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/207932/
+        SUMMARY:Hirakata.rb #8
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/208666/
+        DTSTART:20210326T233000
+        DTEND:20210327T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/208666/
+        SUMMARY:Hirakata.rb #9
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/208815/
+        DTSTART:20210402T233000
+        DTEND:20210403T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/208815/
+        SUMMARY:Hirakata.rb #10
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/209763/
+        DTSTART:20210409T233000
+        DTEND:20210410T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/209763/
+        SUMMARY:Hirakata.rb #11
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://toyamarb.connpass.com/event/208320/
+        DTSTART:20210410T040000
+        DTEND:20210410T090000
+        DESCRIPTION:https://toyamarb.connpass.com/event/208320/
+        SUMMARY:Toyama.rb #62 もくもく会オンライン
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/210773/
+        DTSTART:20210416T233000
+        DTEND:20210417T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/210773/
+        SUMMARY:Hirakata.rb #12
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/210917/
+        DTSTART:20210423T233000
+        DTEND:20210424T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/210917/
+        SUMMARY:Hirakata.rb #13
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/212666/
+        DTSTART:20210507T233000
+        DTEND:20210508T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/212666/
+        SUMMARY:Hirakata.rb #14
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://toyamarb.connpass.com/event/211449/
+        DTSTART:20210508T040000
+        DTEND:20210508T090000
+        DESCRIPTION:https://toyamarb.connpass.com/event/211449/
+        SUMMARY:Toyama.rb #63 もくもく会オンライン
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/213377/
+        DTSTART:20210514T233000
+        DTEND:20210515T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/213377/
+        SUMMARY:Hirakata.rb #15
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/213559/
+        DTSTART:20210521T233000
+        DTEND:20210522T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/213559/
+        SUMMARY:Hirakata.rb #16
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/214308/
+        DTSTART:20210528T233000
+        DTEND:20210529T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/214308/
+        SUMMARY:Hirakata.rb #17
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/214841/
+        DTSTART:20210604T233000
+        DTEND:20210605T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/214841/
+        SUMMARY:Hirakata.rb #18
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://hirakatarb.connpass.com/event/215595/
+        DTSTART:20210611T233000
+        DTEND:20210612T010000
+        DESCRIPTION:https://hirakatarb.connpass.com/event/215595/
+        SUMMARY:Hirakata.rb #19
+        END:VEVENT
+        BEGIN:VEVENT
+        UID:https://toyamarb.connpass.com/event/213277/
+        DTSTART:20210612T040000
+        DTEND:20210612T090000
+        DESCRIPTION:https://toyamarb.connpass.com/event/213277/
+        SUMMARY:Toyama.rb #64 もくもく会オンライン
+        END:VEVENT
+        END:VCALENDAR
+      ICAL
+    end
+
+    it { should eq ical.gsub("\n", "\r\n") }
+  end
+
+  describe ".connpass_terms" do
+    subject { EventCalendar.connpass_terms(current_date) }
+
+    let(:current_date) { Date.parse("2021-06-12") }
+
+    it { should eq [202103, 202104, 202105, 202106, 202107, 202108, 202109] }
+  end
 end
